46aafefbffb8f140069779b24450f9a7d4f84715
Forrest Lee Erickson <forresterickson@comcast.net>
2026-05-25 13:09:45 -0400

    chore: bump firmware patch version to 0.0.13

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,7 @@
 // --- SEMANTIC VERSIONING ---
 #define VERSION_MAJOR 0
 #define VERSION_MINOR 0
-#define VERSION_PATCH 12 // Upgraded to 4-Profile Signal Roaming Matrix System with Screen Diagnostics
+#define VERSION_PATCH 13 // LED behavior update: STA 100/900 blink, SoftAP 500/500 blink
 
 #define LED_BUILTIN 2
 #define SCAN_BUTTON GPIO_NUM_35 
